83e75e9c8b2f1344e198bae485804c710793463b
Duy Nguyen <nduytg@gmail.com>
2016-03-20 10:20:40 +0700

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -35,12 +35,12 @@ def encrypt(mode, IV, input_name, output_name):
 		mode = AES.MODE_CBC
 
 	#Tao key 32bit bang ham bam tu key dinh san
-	#key = hashlib.sha256(MyKey).digest()	#sha256 cho ra 32bytes key
-	key = SHA256.SHA256Hash.digest(MyKey)
+	key = hashlib.sha256(MyKey).digest()	#sha256 cho ra 32bytes key
+	#key = SHA256.SHA256Hash.digest(MyKey)
 	print "Standard key:", key
 	#Bam lai IV cho chuan
-	#IV = hashlib.md5(IV).digest()			#MD5 hash cho ra 16bytes
-	IV = MD5.MD5Hash.digest(IV)
+	IV = hashlib.md5(IV).digest()			#MD5 hash cho ra 16bytes
+	#IV = MD5.MD5Hash.digest(IV)
 	print "Standard IV:", IV
 
 	encryptor = AES.new(key, mode, IV)
@@ -54,16 +54,18 @@ def encrypt(mode, IV, input_name, output_name):
 			f_out.write(struct.pack("<Q",filesize))
 			f_out.write(IV)		#IV dai 16 byte do hash MD5
 
-			#while - do: doc ghi file theo chunk size
-		while True:
-			chunk = f_in.read(BlockSize)
-			if len(chunk) == 0:
-				break
-			elif len(chunk) % 16 != 0:
-				#Padding cho du block
-				chunk = chunk + "@" * (16 - len(chunk) % 16)
-			f_out.write(encryptor.encrypt(chunk))
-	return
+			#while - do: doc ghi file theo block size
+			while True:
+				block = f_in.read(BlockSize)
+				#print "Block:", block
+				if len(block) == 0:
+					break
+				elif len(block) % 16 != 0:
+					#Padding cho du block
+					block = block + "@" * (16 - len(block) % 16)
+					#print "Block with pads:", block
+				f_out.write(encryptor.encrypt(block))
+	#return
 #=================================================================
 
 
@@ -88,6 +90,7 @@ def main(argv):
 		print "encrypt.py –m <mode> –i <IV> <input_file> <output_file>"
 		sys.exit(2)
 
+	print "ARGS:", args
 	#Neu danh sach tham so sau khi tru di cac option
 	#khong phai la dang: "encrypt.py <input_file> <output_file>
 	if( len(args) != 3 and len(args) != 2):
@@ -96,9 +99,9 @@ def main(argv):
 		sys.exit(2)
 
 	#Lay thong tin input_file va output_file
-	input_file = args[1]
-	if(len(args) == 3):
-		output_file = args[2]
+	input_file = args[0]
+	if(len(args) == 2):
+		output_file = args[1]
 	else:
 		#Neu khong co thong so output thi mac dinh la ten fileinput.enc
 		output_file = input_file + ".enc"
@@ -113,6 +116,11 @@ def main(argv):
 		elif opt == "-i":
 			IV = arg
 	#---------------Xu ly xong tham so dong lenh-------------------
+
+	#Goi ham ENCRYPT
+	encrypt(mode, IV, input_file, output_file)
+
+	return
 #=================================================================
 
 if __name__ == "__main__":
